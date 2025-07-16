f551c67ecbc4aed71bb1e040623254213497ab26
fazatech51 <fazalkhanpathan061@gmail.com>
2025-07-17 01:50:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ResumeForm from "./ResumeForm";
+
+function App() {
+  return (
+    <div>
+      <h1>My Resume Maker</h1>
+      <ResumeForm />
+    </div>
+  );
+}
+
+export default App;
